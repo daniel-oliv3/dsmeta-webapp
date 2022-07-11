3b7c4bc29c044a1c317bd9853de7d3f6a61f6508
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-07-11 15:16:51 -0300

    Update components ico

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,10 @@ import NotificationButton from "./components/NotificationButton";
 function App() {
 
   return (
-    <div>
+    <>
       <h1>Olá, Mundo... Sapup3 na área!</h1>
-      <NotificationButton/>
-    </div>
+      <NotificationButton />
+    </>
   )
 }
 
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,11 @@
-
+import icon from '../../assets/img/notification-ico.svg';
 
 function NotificationButton() {
 
     return (
-      <div>
-        <p>Notification Button</p>
-      </div>
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
     )
   }
   

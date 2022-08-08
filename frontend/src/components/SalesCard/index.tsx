e38d25c372d0f6595eb4b3a19cacf1048c638750
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-08 13:26:55 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -23,7 +23,6 @@ function SalesCard() {
 		const dmin = minDate.toISOString().slice(0, 10);
 		const dmax = maxDate.toISOString().slice(0, 10);
 
-		//console.log(dmin);
 
 		axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
 		.then(response => {

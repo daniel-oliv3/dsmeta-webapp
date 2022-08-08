4c17d0d6d9ac1297618f2c8d89ed8ce2cd37fe72
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-08 13:23:23 -0300

    Update project FunFun

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,26 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-ico.svg';
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+
+/*  Function */
+function handleClick(id :number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("Success");
+        });
+}
+
+function NotificationButton( {saleId} : Props) {
 
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId) }>
             <img src={icon} alt="Notificar" />
         </div>
     )
